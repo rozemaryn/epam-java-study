0937c084d2baa68d7727b117bf97401e996191e5
Maria Kostyanetskaya <animalitos@yandex.ru>
2023-05-18 14:38:36 +0300

    ElectronicWatch task, another solution

--- src/main/java/com/kostyanetskaya/epamjavastudy/lesson2/ElectronicWatch.java
+++ src/main/java/com/kostyanetskaya/epamjavastudy/lesson2/ElectronicWatch.java
@@ -1,8 +1,17 @@
 package com.kostyanetskaya.epamjavastudy.lesson2;
-
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Date;
+
+/*
+Перейдите в класс ElectronicWatch и напишите программу, которая получает на вход целое число.
+Это число представляет собой текущее суточное время как количество прошедших с полнуочи секунд.
+Затем программа должна напечатать вывод экрана электронных часов для этого времени.
+
+Гарантируется, что входное значение — неотрицательное.
+
+Формат вывода: h:mm:ss (возможные значения: [0:00:00; 23:59:59]).
+
+Усложненный вариант: попробуйте решить задачу без использования if и циклов.
+ */
 
 public class ElectronicWatch {
     public static String whatTimeItIs (int seconds) {
@@ -13,18 +22,33 @@ public class ElectronicWatch {
         long MM = duration.toMinutesPart();
         long SS = duration.toSecondsPart();
 
-        String time = String.format("%02d:%02d:%02d", HH, MM, SS);
+        String time = String.format("%01d:%02d:%02d", HH, MM, SS);
 
        return time;
     }
 
     public static void main(String[] args) {
-        int i = 86299;
+        int i = 89999;
         String time = whatTimeItIs(i);
         System.out.println(time);
     }
 }
 
 class ElectronicWatchAnotherSolution {
+    public static String whatTimeItIs (int seconds) {
 
+        long HH = (seconds / 3600) % 24;
+        long MM = (seconds%3600) / 60;
+        long SS = seconds%60;
+
+        String time = String.format("%01d:%02d:%02d", HH, MM, SS);
+        return time;
+    }
+
+    public static void main(String[] args) {
+        int i = 600;
+        String time = whatTimeItIs(i);
+        System.out.println(time);
+    }
 }
+
