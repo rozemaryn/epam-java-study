e7ecfc83354698a563aa556d3f617654ef38dfe8
Maria Kostyanetskaya <animalitos@yandex.ru>
2023-06-02 20:09:50 +0300

    task MaxMethod solved

---
+++ src/main/java/com/kostyanetskaya/epamjavastudy/lesson4/tasks/MaxMethod.java
@@ -0,0 +1,19 @@
+package com.kostyanetskaya.epamjavastudy.lesson4.tasks;
+
+public class MaxMethod {
+    public static int max (int [] array) {
+     int max = array[0];
+     for (int value: array) {
+         if (max < value) {
+             max = value;
+         }
+     }
+     return max;
+    }
+
+    public static void main(String[] args) {
+        int[] vals = new int[]{ -2, 0, 10, 5 };
+        int result = MaxMethod.max(vals);
+        System.out.println(result);
+    }
+}
