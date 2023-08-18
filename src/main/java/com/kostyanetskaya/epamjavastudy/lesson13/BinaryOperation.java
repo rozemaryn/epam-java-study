4cf11f468dbea3054989fd1881440b2d8884f096
Maria Kostyanetskaya <animalitos@yandex.ru>
2023-08-18 12:05:57 +0300

    few more enum examples

--- src/main/java/com/kostyanetskaya/epamjavastudy/lesson13/BinaryOperation.java
+++ src/main/java/com/kostyanetskaya/epamjavastudy/lesson13/BinaryOperation.java
@@ -1,40 +1,56 @@
 package com.kostyanetskaya.epamjavastudy.lesson13;
 
 public enum BinaryOperation {
-    PLUS ("+") {
-        public int calculate(int a, int b) {
-            return a + b;
-        }
-    },
-    MINUS ("-") {
-        public int calculate(int a, int b) {
-            return a - b;
-        }
-    },
-    DIVISION ("/") {
-        public int calculate(int a, int b){
-            return a / b;
-        }
-    },
-    MULT ("*") {
-        public int calculate(int a, int b){
-            return a * b;
-        }
-    };
+     //java: No enum constant javax.lang.model.element.Modifier.SEALED
 
-    private final String operation;
+//    PLUS ("+") {
+//        public int calculate(int a, int b) {
+//            return a + b;
+//        }
+//    },
+//    MINUS ("-") {
+//        public int calculate(int a, int b) {
+//            return a - b;
+//        }
+//    },
+//    DIVISION ("/") {
+//        public int calculate(int a, int b){
+//            return a / b;
+//        }
+//    },
+//    MULT ("*") {
+//        public int calculate(int a, int b){
+//            return a * b;
+//        }
+//    };
+//
+//    private final String operation;
+//
+//    public abstract int calculate(int a, int b);
+//
+//    BinaryOperation(String operation) {
+//        this.operation = operation;
+//    }
+//
+//    public static void main(String[] args) {
+//        int x = 10;
+//        int y = 2;
+//        for (BinaryOperation op : BinaryOperation.values())
+//            System.out.println(x + " " + op.operation + " " + y
+//                    + " = " + op.calculate(x, y));
+//    }
+}
 
-    public abstract int calculate(int a, int b);
-
-    BinaryOperation(String operation) {
-        this.operation = operation;
-    }
-
+enum Numbers {ONE, TWO, THREE, FOUR, FIVE}
+class Quest9 {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 2;
-        for (BinaryOperation op : BinaryOperation.values())
-            System.out.println(x + " " + op.operation + " " + y
-                    + " = " + op.calculate(x, y));
+        Numbers n1 = Numbers.ONE;
+        Numbers n2 = Numbers.ONE;  //Строка1
+        if (n1 == n2) {
+            System.out.print ("true");
+        } else {
+            System.out.print ("false");
+        }
+        System.out.println( Numbers.FIVE.ordinal() );  //Строка2
     }
 }
--- src/main/java/com/kostyanetskaya/epamjavastudy/lesson13/Operation.java
+++ src/main/java/com/kostyanetskaya/epamjavastudy/lesson13/Operation.java
@@ -1,34 +1,50 @@
 package com.kostyanetskaya.epamjavastudy.lesson13;
 
 public enum Operation {
-    PLUS {
-        double eval(double x, double y) {
-            return x + y;
-        }
-    },
-    MINUS {
-        double eval(double x, double y) {
-            return x - y;
-        }
-    },
-    TIMES {
-        double eval(double x, double y) {
-            return x * y;
-        }
-    },
-    DIVIDED_BY {
-        double eval(double x, double y) {
-            return x / y;
-        }
-    };
+    //java: No enum constant javax.lang.model.element.Modifier.SEALED
+//    PLUS {
+//        double eval(double x, double y) {
+//            return x + y;
+//        }
+//    },
+//    MINUS {
+//        double eval(double x, double y) {
+//            return x - y;
+//        }
+//    },
+//    TIMES {
+//        double eval(double x, double y) {
+//            return x * y;
+//        }
+//    },
+//    DIVIDED_BY {
+//        double eval(double x, double y) {
+//            return x / y;
+//        }
+//    };
+//
+//    abstract double eval(double x, double y);
+//
+//    public static void main(String[] args) {
+//        double x = 2.0;
+//        double y = 4.0;
+//        for (Operation op : Operation.values())
+//            System.out.println(x + " " + op + " " + y
+//                    + " = " + op.eval(x, y));
+//    }
+}
 
-    abstract double eval(double x, double y);
+enum PrinterType {
 
-    public static void main(String[] args) {
-        double x = 2.0;
-        double y = 4.0;
-        for (Operation op : Operation.values())
-            System.out.println(x + " " + op + " " + y
-                    + " = " + op.eval(x, y));
+    DOTMATRIX(5),
+    INKJET(10),
+    LASER(50);
+
+    private int pagePrintCapacity;
+    private PrinterType(int pagePrintCapacity)  {
+        this.pagePrintCapacity = pagePrintCapacity;
+    }
+    public int getPrintPageCapacity() {
+        return pagePrintCapacity;
     }
 }
